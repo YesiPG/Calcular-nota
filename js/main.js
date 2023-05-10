ddf95b3c09032bd9254fd2d1114b9b5a925d01e8
Yesica Perlaza <perlazayesik@gmail.com>
2023-05-09 22:27:50 -0500

    probando opciones en main.js

--- js/main.js
+++ js/main.js
@@ -7,9 +7,33 @@
   const infoAgregar = d.querySelector(".contInfo");
   const nota = d.querySelector(".contFormLabelNota");
   const porcentaje = d.querySelector(".contFormLabelPorcentaje");
+  const nota1 = d.querySelector(".contFormLabelNota1");
+  const porcentaje1 = d.querySelector(".contFormLabelPorcentaje1");
+  const nota2 = d.querySelector(".contFormLabelNota2");
+  const porcentaje2 = d.querySelector(".contFormLabelPorcentaje2");
+  const nota3 = d.querySelector(".contFormLabelNota3");
+  const porcentaje3 = d.querySelector(".contFormLabelPorcentaje3");
+  const contenedor = [...d.querySelectorAll(".contFormAddCont")];
+  let contador = 0;
 
   botonAgregar.addEventListener("click", (e) => {
-    
+
+    contador++;
+
+    if(contador === 1){
+        contenedor[1].classList.remove("contFormAddContShow");
+    }
+    if(contador === 2){
+        contenedor[2].classList.remove("contFormAddContShow");
+
+    }
+    if(contador === 3){
+        contenedor[3].classList.remove("contFormAddContShow");
+
+    }
+    if(contador > 3){
+        infoAgregar.innerHTML = "Superaste el limite de notas que puedes agregar";
+    }
   });
 
   botonCalcular.addEventListener("click", (e) => {
